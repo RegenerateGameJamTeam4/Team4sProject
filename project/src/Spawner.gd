479f0ff06202e78182f25440ff861ecfaba87764
RainerW <rainerweston@gmail.com>
2022-02-19 22:59:41 +1300

    spawner

---
+++ project/src/Spawner.gd
@@ -0,0 +1,27 @@
+extends Spatial
+
+var assetlist = [preload("res://src/Plants/big_tree_001.tscn"),
+				preload("res://src/Plants/big_tree_003.tscn"),
+				preload("res://src/Plants/big_tree_004.tscn"),
+				preload("res://src/Plants/flat_shrub_001.tscn"),
+				preload("res://src/Plants/square_shrub_001.tscn"),
+				preload("res://src/Plants/small_plant_001.tscn"),
+				]
+
+export var wait_range:Vector2
+export var spawn_range:Vector2
+
+func _ready():
+	$SpawnTimer.start()
+	
+
+func spawn(random=true):
+	var pick = randi()%assetlist.size()
+	var new_asset = assetlist[pick].instance()
+	new_asset.transform.origin = Vector3(rand_range(spawn_range.x,spawn_range.y), 0, rand_range(-0.5,0.5))
+	add_child(new_asset)
+
+
+func _on_SpawnTimer_timeout():
+	$SpawnTimer.wait_time = rand_range(wait_range.x,wait_range.y)
+	spawn()
