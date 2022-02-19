d154a93f057b53aecfe9a519bd595a8ad7b8e225
RainerW <rainerweston@gmail.com>
2022-02-19 16:55:27 +1300

    player collision masks and movement

--- project/src/actors/Player.gd
+++ project/src/actors/Player.gd
@@ -1,14 +1,17 @@
 extends Area
 
-var speed = 5
+var speed = 0.1
 var target_location:Vector3 = Vector3(0,0,0)
 
 func _ready():
 	pass
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 func _physics_process(delta):
-	pass
-	#if global_transform.origin != target_location:
-		#look_at(target_location, Vector3.UP)
-		#transform.origin.z+= speed*delta
+	#pass
+	if global_transform.origin != target_location:
+		look_at(target_location, Vector3.UP)
+		#transform.origin += (transform.origin.direction_to(target_location)*speed)*delta
+		transform.origin = transform.origin.linear_interpolate(target_location, 0.35*delta)
+		transform.origin.y = 0;
+
