4d0ee8d140a3e6fc3b0cc2bb5e43ae52dc16c7b5
RainerW <rainerweston@gmail.com>
2022-02-21 20:36:57 +1300

    Adding nice particles to main menu

--- project/src/scenes/main_menu.gd
+++ project/src/scenes/main_menu.gd
@@ -9,6 +9,12 @@ var faded_in = false
 
 func _ready():
 	manager._environment.environment = environment
+	
+	$BreatheTween.interpolate_property($MeshInstance, "transform:scale",
+		Vector3(1, 1, 1), Vector3(1.5, 1.5, 1.5), 1,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$BreatheTween.start()
+	
 	$AnimationPlayer.play("fade_in")
 	yield($AnimationPlayer,"animation_finished")
 	faded_in=true
@@ -20,6 +26,9 @@ func _process(delta):
 			begin = true
 			manager._musicplayer.play()
 			$AnimationPlayer.play("fade_out")
+
+func _physics_process(delta):
+	pass
 
 func start_game():
 	manager.load_scene(manager.game_path,"game")
